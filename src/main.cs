31cd19b10023098b0a3ff8a01c590764826e350c
epeedoh <epeedoh@yahoo.fr>
2025-05-14 21:40:02 +0000

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,8 @@ using System.Net.Sockets;
 // Uncomment this line to pass the first stage
 Console.Write("$ ");
 
+
 // Wait for user input
-Console.ReadLine();
+var command = Console.ReadLine();
+
+Console.WriteLine($"{command}: command not found");
