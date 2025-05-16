57c3681e893dfa7fb12e6cba94f05a1d484088f3
epeedoh <epeedoh@yahoo.fr>
2025-05-16 22:34:00 +0000

    [AJout de la code de sortit]

--- src/main.cs
+++ src/main.cs
@@ -16,11 +16,14 @@ while (true)
     // Wait for user input
     String? command = Console.ReadLine()?.Trim();
 
+    //if (command == "exit")
+    //    return;
 
     if (string.IsNullOrEmpty(command) || command.ToLower() == "exit")
-       break;
+       return;
+
+   
 
     Console.WriteLine($"{command}: command not found");
-
 
 }
