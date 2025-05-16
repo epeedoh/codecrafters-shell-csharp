8b70385a8cbdfd1ed4a574ed79d709143181bcb0
epeedoh <epeedoh@yahoo.fr>
2025-05-16 18:27:27 +0000

    [AJout de la boucle MAJ]

--- src/main.cs
+++ src/main.cs
@@ -1,11 +1,26 @@
 using System.Net;
 using System.Net.Sockets;
 
-// Uncomment this line to pass the first stage
-Console.Write("$ ");
 
 
-// Wait for user input
-var command = Console.ReadLine();
 
-Console.WriteLine($"{command}: command not found");
+
+
+
+
+while (true)
+{
+    // Uncomment this line to pass the first stage
+    Console.Write("$ ");
+
+    // Wait for user input
+    String? command = Console.ReadLine()?.Trim();
+
+
+    if (string.IsNullOrEmpty(command) || command.ToLower() == "exit")
+       break;
+
+    Console.WriteLine($"{command}: command not found");
+
+
+}
